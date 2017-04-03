478ec69221f17cebed49cba0c6d17d1a3db45dc8
Luis Rodriguez <rodriguez.arguijo@gmail.com>
2017-04-02 23:53:17 -0600

    Agregando la fecha de firma

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -1,12 +1,16 @@
 import java.math.BigDecimal;
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
 
 public class JavaClass {
 
 	public static void main(String[] args) {
 		System.out.println("hola Victoria, como estas.");
-		System.out.println("Firma: Luis");
+		System.out.println("Firma: Luis "+ Calendar.getInstance().getTime());
 
 		int[] numeris = { 1, 2, 3, 4, 5, 6, 7, 9, 10 };
 		int sumador = 0;
